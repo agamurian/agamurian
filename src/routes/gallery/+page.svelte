d4b39676ba1ebccf1ee8ea7c4a50fe42c1960fb4
Andrey Golovin <a.golovin@blackhub.games>
2025-12-27 23:05:13 +0300

    add gallery

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,8 +1,54 @@
-	<section class="section">
-		<div class="main-block">
+<article class="p-5">
+<br/>
+<br/>
+	<img  class='img' src="/gallery/nine.webp"/>
+	<div class="text">the nine</div>
+	<img  class='img' src="/gallery/3.webp"/>
+	<div class="text">image eight</div>
+	<img  class='img' src="/gallery/4.webp"/>
+	<div class="text">the four</div>
+	<img class='img' src="/gallery/6.webp"/>
+	<div class="text">the five</div>
+	<img class='img' src="/gallery/5.webp"/>
+	<div class="text">the six</div>
+	<img class='img' src="/gallery/w1.webp"/>
+	<div class="text">capsule one</div>
+	<img  class='img' src="/gallery/w7.webp"/>
+	<div class="text">capsule one</div>
+	<img  class='img' src="/gallery/w8.webp"/>
+	<div class="text">capsule one</div>
+	<img  class='img' src="/gallery/w9.webp"/>
+	<div class="text">capsule one</div>
+	<img  class='img' src="/gallery/2.webp"/>
+	<div class="text">Coffee and Orange</div>
+</article>
 
-			<h1 class="header">Andrey Golovin</h1>
-			<p>Software Developer and an Artist</p>
-			<nobr><a href='/cv_andrey_golovin.pdf' >CV.PDF</a></nobr>
-	</div>
-	</section>
+<style lang="css">
+
+	.separator {
+		color: antiquewhite;
+		opacity: 0.1;
+		font-family: 'Inconsolata';
+		font-size: 3.0vw;
+		margin: 0.5rem;
+		margin-bottom: 4.0rem;
+		text-align: center;
+	}
+	.text {
+		opacity: 0.6;
+		font-family: 'Inconsolata';
+		font-weight: 600;
+		font-size: 0.9rem;
+		margin: 1rem;
+		margin-bottom: 4rem;
+		text-align: center;
+	}
+	.img {
+		max-height: 75vh;
+		outline: none;
+		border: none;
+		display: block;
+		margin:auto;
+		border-radius: 0.5rem;
+	}
+	</style>
