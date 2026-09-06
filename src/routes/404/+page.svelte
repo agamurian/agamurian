10a4dae292472dfc3208131eae61d7c7e1344573
agamurian <agamurian@gmail.com>
2026-09-06 07:52:55 +0200

    fix 404

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,31 +1,5 @@
-	<section class="section">
-		<div class="main-block">
+<div class="main-block">
 
-			<h1 class="header">404</h1>
-			<nobr>No such page found</nobr>
-	</div>
-	</section>
-
-	<section class="section">
-	</section>
-	<section class="section">
-		<h5 class="header" >
-				Check out the gallery:
-			</h5>
-			<br/>
-			<div class="cards">
-				<div class="card" style="background-image: url('/back6.webp'); background-position: center; background-size: cover; height: 200px">
-					<div class="card-text-hover">
-						<h2 class="header">Forest</h2>
-						<p>optimised location with lod, impostors, custom shaders and optimizations</p>
-					</div>
-				</div>
-				<div class="card" style="background-image: url('/back3.webp'); background-position: center; background-size: cover; height: 200px">
-					<div class="card-text-hover">
-						<h2 class="header">3D viewer</h2>
-						<p>Interactive rotatable, page-aware 3d viewer</p>
-					</div>
-				</div>
-			</div>
-		</section>
-
+    <h1 class="header">404</h1>
+    <nobr>No such page found</nobr>
+</div>
